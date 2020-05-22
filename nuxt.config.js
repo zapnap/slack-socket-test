5713b53bf72f07459570c3d818481f153b8c54fa
Nick Plante <nap@zerosum.org>
2020-05-22 10:01:37 -0400

    universal test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  mode: 'universal',
+  server: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3000
+  },
   head: {
     meta: [
       { charset: 'utf-8' },
--- server.js
+++ server.js
@@ -15,17 +15,20 @@ const io = SocketIO(server)
 const config = require('./nuxt.config.js')
 config.dev = !isProd
 
-const nuxt = new Nuxt(config)
-// Start build process in dev mode
-if (config.dev) {
-  const builder = new Builder(nuxt)
-  builder.build()
-}
-app.use(nuxt.render)
+async function start() {
+  const nuxt = new Nuxt(config)
+  // Start build process in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    builder.build()
+  }
+  app.use(nuxt.render)
 
-// Listen the server
-server.listen(port, '0.0.0.0')
-console.log('Server listening on localhost:' + port) // eslint-disable-line no-console
+  // Listen the server
+  server.listen(port, '0.0.0.0')
+  console.log('Server listening on localhost:' + port) // eslint-disable-line no-console
+}
+start()
 
 // Socket.io
 const messages = []
